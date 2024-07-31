17059d3742af258b0773fc630f97ee220f574b87
Bruce Caton <catonblt@gmail.com>
2024-07-31 14:13:55 -0400

    Finished Rock Paper Scissors UI.

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let randomNumber;
+let playing = true;
 let computerChoice;
-
+let humanScore = 0;
+let computerScore = 0;
 let humanChoice;
+let buttonClicked = false;
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
 
+let HScore = document.querySelector(".H-Score");
+let CScore = document.querySelector(".C-Score");
+
+const buttons = document.querySelectorAll("button");
+
+if (humanScore || computerScore < 5) {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      humanChoice = button.className;
+      console.log(`Human Picked: ${humanChoice}`);
+      getComputerChoice();
+      playRound(humanChoice, computerChoice);
+    });
+  });
+}
 
 function getComputerChoice() {
   randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -19,66 +40,60 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  humanChoice = prompt("Choose Rock, Paper, or Scissors:").toUpperCase();
-
-  while (
-    humanChoice !== "ROCK" &&
-    humanChoice !== "PAPER" &&
-    humanChoice !== "SCISSORS"
-  ) {
-    humanChoice = prompt(
-      ` ${humanChoice} is not one of the options. Try Again... Choose Rock, Paper, or Scissors:`
-    ).toUpperCase();
-  }
-  return console.log(`You Picked: ${humanChoice}`);
-}
-
-
-
-function playGame() {
-
-  let computerScore = 0;
-  let humanScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {
+  if (playing) {
     if (
       (humanChoice === "ROCK" && computerChoice === "PAPER") ||
       (humanChoice === "PAPER" && computerChoice === "SCISSORS") ||
       (humanChoice === "SCISSORS" && computerChoice === "ROCK")
     ) {
       computerScore += 1;
-      return console.log(
-        `${computerChoice} beats ${humanChoice}. "Computer wins!!"`
-      );
+      CScore.textContent = "";
+      CScore.textContent = `Computer Score: ${computerScore}`;
+
+      let results = document.querySelector(".results");
+      results.textContent = "";
+      let content = document.createElement("div");
+      content.classList.add("content");
+      content.textContent = `${computerChoice} beats ${humanChoice}. Computer wins.`;
+      results.appendChild(content);
     }
-  
+
     if (
       (humanChoice === "PAPER" && computerChoice === "ROCK") ||
       (humanChoice === "SCISSORS" && computerChoice === "PAPER") ||
       (humanChoice === "ROCK" && computerChoice === "SCISSORS")
     ) {
       humanScore += 1;
-      return console.log(`${humanChoice} beats ${computerChoice}. Human wins!!`);
-    } else {
-      console.log("It was a draw!");
+      HScore.textContent = "";
+      HScore.textContent = `Human Score: ${humanScore}`;
+
+      let results = document.querySelector(".results");
+      results.textContent = "";
+      let content = document.createElement("div");
+      content.classList.add("content");
+      content.textContent = `${humanChoice} beats ${computerChoice}. Human wins.`;
+      results.appendChild(content);
+    } else if (humanChoice === computerChoice) {
+      let results = document.querySelector(".results");
+      results.textContent = "";
+      let content = document.createElement("div");
+      content.classList.add("content");
+      content.textContent = `It's a draw.`;
+      results.appendChild(content);
     }
   }
-
-  for (let i = 1; i <= 5; i++) {
-    getHumanChoice();
-    getComputerChoice();
-    playRound(computerChoice, humanChoice);
+  if (computerScore === 5) {
+    results = document.querySelector(".results");
+    results.textContent += " ---Computer wins the Tournament!!";
+    playing = false;
+    computerScore += 1;
   }
-
-  if (humanScore > computerScore) {
-    return console.log(`Human won ${humanScore} rounds. Human wins!`);
-  } else if (computerScore > humanScore) {
-    return console.log(`Computer won ${computerScore} rounds. Computer wins!`);
-  } else {
-    return console.log("It's a draw!!");
+  if (humanScore === 5) {
+    results = document.querySelector(".results");
+    results.textContent += " --- Human wins the Tournament!!";
+    playing = false;
+    humanScore += 1;
   }
 }
-
-playGame();
 
